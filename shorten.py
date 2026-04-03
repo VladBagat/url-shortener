4591310c74f4d13209682c28d86d5b8839d6c63b
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2026-04-03 16:33:49 +0100

    remove deduplication logic for simpler querying

--- shorten.py
+++ shorten.py
@@ -4,8 +4,6 @@ import string
 import random
 import time
 from typing import Any, Dict
-from boto3.dynamodb.conditions import Attr
-from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('urls')
@@ -34,39 +32,23 @@ def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
             "body": json.dumps({"error": "Invalid request body. Expected {'url': '...'}"})
         }
 
-    MAX_RETRIES = 5
-    for _ in range(MAX_RETRIES):
-        short_url = get_short_url()
-        
-        try:
-            table.put_item(
-                Item={
-                    "short_url": short_url,
-                    "long_url": long_url,
-                    "creation_time": int(time.time()),
-                },
-                ConditionExpression=Attr('short_url').not_exists()
-            )
-            
-            return {
-                "statusCode": 200,
-                "headers": headers,
-                "body": json.dumps({"short_url": short_url, "long_url": long_url})
-            }
-            
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-                continue
-            else:
-                raise 
+    short_url = get_short_url()
+
+    table.put_item(
+        Item={
+            "short_url": short_url,
+            "long_url": long_url,
+            "creation_time": int(time.time()),
+        }
+    )
 
     return {
-        "statusCode": 500,
+        "statusCode": 200,
         "headers": headers,
-        "body": json.dumps({"error": "Could not generate a unique short URL."})
+        "body": json.dumps({"short_url": short_url, "long_url": long_url})
     }
 
 def get_short_url() -> str:
-    URL_LENGTH = 6
+    URL_LENGTH = 8
     ALPHABET = string.ascii_letters + string.digits
     return ''.join(random.choices(ALPHABET, k=URL_LENGTH))
